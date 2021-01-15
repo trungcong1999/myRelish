4c32ea84ff383d569bc8a37dc6c064f84233c029
Hoàng Trung Công <37680352+trungcong1999@users.noreply.github.com>
2021-01-15 21:01:50 +0700

    insert

---
+++ MyRelish/app/src/main/java/com/vn/itplus/myrelish/RegisterActivity.java
@@ -0,0 +1,144 @@
+package com.vn.itplus.myrelish;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import 	android.app.ProgressDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+    public static final String TAG = RegisterActivity.class.getSimpleName();
+    EditText editName,editEmail,editPass,editRepass;
+    Button btnCreate,btnClose;
+    private ProgressDialog pDialog;
+    public static final String REGISTER_URL = "http://localhost:8080/apiRelish/user/register";
+
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_EMAIL = "email";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mapping();
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editPass.getText().length()<8){
+                    Toast.makeText(RegisterActivity.this, "Password phải lớn hơn 8 ký tự", Toast.LENGTH_SHORT).show();
+                }else{
+                    if (!editPass.getText().equals(editRepass.getText())){
+                        Toast.makeText(RegisterActivity.this, "Re-Password không đúng", Toast.LENGTH_SHORT).show();
+                    }else{
+                        String username = editName.getText().toString().trim();
+                        String password = editPass.getText().toString().trim();
+                        String email = editEmail.getText().toString().trim();
+                        registerUser(username,password,email);
+                    }
+                }
+            }
+        });
+    }
+    private boolean checkEditText(EditText editText) {
+        if (editText.getText().toString().trim().length() > 0)
+            return true;
+        else {
+            editText.setError("Vui lòng nhập dữ liệu!");
+        }
+        return false;
+    }
+    private boolean isValidEmail(String target) {
+        if (target.matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+"))
+            return true;
+        else {
+            editEmail.setError("Email sai định dạng!");
+        }
+        return false;
+    }
+    private void registerUser(final String username, final String password, final String email) {
+
+        if (checkEditText(editName) && checkEditText(editPass) && checkEditText(editEmail) && isValidEmail(email)) {
+            pDialog.show();
+            StringRequest registerRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d(TAG, response);
+                            String message = "";
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                if (jsonObject.getInt("success") == 1) {
+
+                                    message = jsonObject.getString("message");
+                                    Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_LONG).show();
+                                    //Start LoginActivity
+                                    Intent intent = new Intent(RegisterActivity.this, SimpleLoginActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    message = jsonObject.getString("message");
+                                    Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException error) {
+                                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                            }
+                            pDialog.dismiss();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            VolleyLog.d(TAG, "Error: " + error.getMessage());
+                            pDialog.dismiss();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put(KEY_USERNAME, username);
+                    params.put(KEY_PASSWORD, password);
+                    params.put(KEY_EMAIL, email);
+                    return params;
+                }
+
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(registerRequest);
+        }
+    }
+    private void mapping() {
+        editName = findViewById(R.id.txt_name_r);
+        editEmail = findViewById(R.id.txt_email_r);
+        editPass = findViewById(R.id.txt_pass_r);
+        editRepass = findViewById(R.id.txt_Rpass_r);
+        btnCreate = findViewById(R.id.btn_cacount);
+        btnClose = findViewById(R.id.btnClose);
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Đang đăng ký...");
+        pDialog.setCanceledOnTouchOutside(false);
+    }
+}
